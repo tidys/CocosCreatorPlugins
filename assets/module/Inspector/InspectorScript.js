8fc9411ef30819bb65570a26c774c14c522ca8f4
许彦峰 <xu_yanfeng@126.com>
2018-01-18 18:04:45 +0800

    no message

--- assets/module/Inspector/InspectorScript.js
+++ assets/module/Inspector/InspectorScript.js
@@ -1,6 +1,10 @@
 module.exports = {
     // cc.require('InspectorScript').inspectorSupport();
     inspectorSupport() {
+        let msgType = {
+            nodeListInfo: 1,// 节点信息
+            notSupport: 0,// 不支持的游戏
+        };
         let postData = {
             scene: {
                 "sceneName": {
@@ -12,6 +16,7 @@ module.exports = {
             },
         };
 
+        // 收集节点信息
         function getNodeChildren(node, data) {
             let nodeName = node.name;
             console.log("nodeName: " + nodeName);
@@ -41,10 +46,15 @@ module.exports = {
             let nodeChildren = node.getChildren();
             for (let i = 0; i < nodeChildren.length; i++) {
                 let childItem = nodeChildren[i];
-                let childName = childItem.name;
-                console.log("childName: " + childName);
+                // let childName = childItem.name;
+                // console.log("childName: " + childName);
                 getNodeChildren(childItem, data[nodeName].children);
             }
+
+        }
+
+        function sendMsgToDevTools(type, msg) {
+            window.postMessage({type: type, msg: msg}, "*");
 
         }
 
@@ -62,9 +72,11 @@ module.exports = {
                 getNodeChildren(item, postData.scene[sceneName].children);
             }
 
-            console.log(postData);
+            // console.log(postData);
+            sendMsgToDevTools(msgType.nodeListInfo, postData);
         } else {
             postData.scene = null;
+            sendMsgToDevTools(msgType.notSupport, "不支持调试游戏!");
         }
     },
 
