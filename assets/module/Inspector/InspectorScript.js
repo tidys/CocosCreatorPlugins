925d0de93f0bb1d83b845c61ad5f256d22f83cd5
许彦峰 <xu_yanfeng@126.com>
2018-01-22 11:35:42 +0800

    注入脚本的逻辑

--- assets/module/Inspector/InspectorScript.js
+++ assets/module/Inspector/InspectorScript.js
@@ -1,33 +1,84 @@
 module.exports = {
     // cc.require('InspectorScript').inspectorSupport();
     inspectorSupport() {
+        //-------------------------------->>  BEGAN  <<-----------------------------------------------
         let msgType = {
             nodeListInfo: 1,// 节点信息
             notSupport: 0,// 不支持的游戏
         };
         let postData = {
             scene: {
-                "sceneName": {
-                    name: "",
-                    children: {
-                        // 节点的名字作为key
-                    }
-                }
+                name: "",
+                children: []
             },
+        };
+        window.inspectorGameMemoryStorage = window.inspectorGameMemoryStorage || {};
+
+        // 收集组件信息
+        function getNodeComponentsInfo(node) {
+            let ret = [];
+            let nodeComp = node._components;
+            for (let i = 0; i < nodeComp.length; i++) {
+                let itemComp = nodeComp[i];
+                window.inspectorGameMemoryStorage[itemComp.uuid] = itemComp;
+                ret.push({
+                    uuid: itemComp.uuid,
+                    type: itemComp.constructor.name,
+                    name: itemComp.name,
+                });
+            }
+            return ret;
+        }
+
+        // 获取节点信息
+        window.getNodeInfo = function (uuid) {
+            let node = window.inspectorGameMemoryStorage[uuid];
+            if (node) {
+                let nodeComp = getNodeComponentsInfo(node);
+                let nodeData = {
+                    uuid: node.uuid,
+                    name: node.name,
+                    x: node.x,
+                    y: node.y,
+                    zIndex: node.zIndex,
+                    childrenCount: node.childrenCount,
+                    children: [],
+                    width: node.width,
+                    height: node.height,
+                    active: node.active,
+                    color: node.color.toCSS(),
+                    opacity: node.opacity,
+                    rotation: node.rotation,
+                    rotationX: node.rotationX,
+                    rotationY: node.rotationY,
+                    anchorX: node.anchorX,
+                    anchorY: node.anchorY,
+                    scaleX: node.scaleX,
+                    scaleY: node.scaleY,
+                    skewX: node.skewX,
+                    skewY: node.skewY,
+                    components: nodeComp
+                };
+                let str = JSON.stringify(nodeData);
+                console.log("node:\n" + str);
+            } else {
+                // 未获取到节点数据
+                console.log("未获取到节点数据");
+            }
         };
 
         // 收集节点信息
         function getNodeChildren(node, data) {
-            let nodeName = node.name;
-            // console.log("nodeName: " + nodeName);
-            data[nodeName] = {
+            // console.log("nodeName: " + node.name);
+            let nodeComp = getNodeComponentsInfo(node);
+            let nodeData = {
                 uuid: node.uuid,
-                name: nodeName,
+                name: node.name,
                 x: node.x,
                 y: node.y,
                 zIndex: node.zIndex,
                 childrenCount: node.childrenCount,
-                children: {},
+                children: [],
                 width: node.width,
                 height: node.height,
                 active: node.active,
@@ -42,42 +93,52 @@ module.exports = {
                 scaleY: node.scaleY,
                 skewX: node.skewX,
                 skewY: node.skewY,
+                components: nodeComp
             };
+            window.inspectorGameMemoryStorage[node.uuid] = node;
             let nodeChildren = node.getChildren();
             for (let i = 0; i < nodeChildren.length; i++) {
                 let childItem = nodeChildren[i];
-                // let childName = childItem.name;
-                // console.log("childName: " + childName);
-                getNodeChildren(childItem, data[nodeName].children);
+                // console.log("childName: " + childItem.name);
+                getNodeChildren(childItem, nodeData.children);
             }
-
+            data.push(nodeData);
         }
 
-        function sendMsgToDevTools(type, msg) {
+        window.sendMsgToDevTools = function (type, msg) {
             window.postMessage({type: type, msg: msg}, "*");
+        };
 
+        // 检测是否包含cc变量
+        try {
+            let cocosInspectorTestVar = cc;
+        } catch (e) {
+            window.sendMsgToDevTools(msgType.notSupport, "不支持调试游戏!");
+            return;
         }
 
         let scene = cc.director.getScene();
         if (scene) {
-            postData.scene = {};
-            let sceneName = scene.name;
-            postData.scene[sceneName] = {};
-            postData.scene[sceneName].name = sceneName;
-            postData.scene[sceneName].children = {};
+            postData.scene = {
+                type: 1,// 标识类型
+                uuid: scene.uuid,
+                name: scene.name,
+                children: [],
+            };
+            window.inspectorGameMemoryStorage[scene.uuid] = scene;
 
             let sceneChildren = scene.getChildren();
             for (let i = 0; i < sceneChildren.length; i++) {
-                let item = sceneChildren[i];
-                getNodeChildren(item, postData.scene[sceneName].children);
+                let node = sceneChildren[i];
+                getNodeChildren(node, postData.scene.children);
             }
-
-            // console.log(postData);
-            sendMsgToDevTools(msgType.nodeListInfo, postData);
+            console.log(postData);
+            window.sendMsgToDevTools(msgType.nodeListInfo, postData);
         } else {
             postData.scene = null;
-            sendMsgToDevTools(msgType.notSupport, "不支持调试游戏!");
+            window.sendMsgToDevTools(msgType.notSupport, "不支持调试游戏!");
         }
+        //-------------------------------->>  END  <<-----------------------------------------------
     },
 
 };
