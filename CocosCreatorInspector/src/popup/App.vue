7d5bdd09b26d6b39f699ef0c06ff277b3a9407fd
许彦峰 <xu_yanfeng@126.com>
2018-02-07 14:49:00 +0800

    修改popup界面

--- CocosCreatorInspector/src/popup/App.vue
+++ CocosCreatorInspector/src/popup/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <div id="app" style="width: 200px;">
-    <h3>{{label}}</h3>
-    <br/>
-    <!--<el-button @click="onBtnClickGitHub">ffffff</el-button>-->
-    <div style="height: 20px">
-      <a href="https://github.com/tidys/CocosCreatorPlugins/tree/master/CocosCreatorInspector" target="_blank">GitHub开源地址</a>
+  <div id="app" style="width: auto;">
+    <h3>{{title}}</h3>
+    <div style="text-align: center;width: 100%; color: #6d6d6d;">
+      <span>支持作者</span>
     </div>
     <br/>
-    <div style="height: 50px">
-      <a href="https://jq.qq.com/?_wv=1027&k=5SdPdy2" target="_blank">
-        点击链接加入群 <br/>
-        【CocosCreator插件开发交流】</a>
+    <div style="margin:0 auto;width:100%;">
+      <div style="width: 200px; margin: 0 auto;" v-show="isShowMoneyPng">
+        <img style="width: 100%;height: auto;" src="static/images/money.jpg">
+      </div>
+    </div>
+    <br/>
+    <div id="foot" style="height: 30px;">
+      <span style="font-size: 14px;float: left;text-align: center;line-height: 30px;color: #6d6d6d;">联系方式:</span>
+      <div style="height: 100%;float: right;margin-right: 10px;">
+        <a href="https://github.com/tidys/CocosCreatorPlugins/tree/master/CocosCreatorInspector" target="_blank">
+          <img src="static/images/github.png" style="height: 100%;">
+        </a>
+      </div>
+      <div style="height: 100%;float: right;margin-right: 10px;">
+        <a href="https://jq.qq.com/?_wv=1027&k=5SdPdy2" target="_blank">
+          <img src="static/images/qq.png" style="height: 100%;">
+        </a>
+      </div>
     </div>
   </div>
-
 </template>
 
 <script>
+  import Vue from 'vue'
+  import 'vue-awesome/icons/flag'
+  import 'vue-awesome/icons'
+  import Icon from 'vue-awesome/components/Icon'
+
+  Vue.component('icon', Icon);
+
   export default {
     name: "app",
     data() {
       return {
-        label: "Cocos Creator Inspector插件",
+        title: "cc-inspector",
+        isShowMoneyPng: true,
       }
     },
     methods: {
