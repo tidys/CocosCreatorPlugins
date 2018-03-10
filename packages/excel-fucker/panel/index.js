f98de8ffecc976d4acaaf991d31670d9ffd4fe1a
许彦峰 <774177933@qq.com>
2018-03-10 18:15:58 +0800

    第一版完工

--- packages/excel-fucker/panel/index.js
+++ packages/excel-fucker/panel/index.js
@@ -138,15 +138,57 @@ Editor.Panel.extend({
                         return;
                     }
                 },
+                _saveToJavaScript(excelData, itemSheet) {
+                    // 以key的形式作为索引值
+                },
+                _saveToJson(excelData, itemSheet) {
+                    let title = excelData[0];
+                    let desc = excelData[1];
+                    let saveData = [];
+                    for (let i = 2; i < excelData.length; i++) {
+                        let lineData = excelData[i];
+                        let saveLineData = {};
+                        for (let j = 0; j < title.length; j++) {
+                            let key = title[j];
+                            let value = lineData[j];
+                            // console.log("" + value);
+                            saveLineData[key] = value;
+                        }
+                        saveData.push(saveLineData);
+                    }
+                    // 保存
+                    let saveStr = JSON.stringify(saveData);
+                    let saveFileFullPath = path.join(this.jsonSavePath, itemSheet.sheet + ".json");
+                    fs.writeFileSync(saveFileFullPath, saveStr);
+                    console.log("转换成json文件成功:" + saveFileFullPath);
+                },
                 onBtnClickGenJson() {
                     console.log("onBtnClickGenJson");
                     for (let k in this.excelArray) {
                         let itemSheet = this.excelArray[k];
                         if (itemSheet.isUse) {
+                            let excelData = nodeXlsx.parse(itemSheet.fullPath);
+                            let sheetData = null;
+                            for (let j in excelData) {
+                                if (excelData[j].name === itemSheet.sheet) {
+                                    sheetData = excelData[j].data;
+                                }
+                            }
+                            if (sheetData) {
+                                if (sheetData.length > 2) {
+                                    this._saveToJson(sheetData, itemSheet);
+                                } else {
+                                    console.log("行数低于2行,无效sheet:" + itemSheet.sheet);
+                                }
+                            } else {
+                                console.log("未发现数据");
+                            }
+
                         } else {
                             console.log("文件未启用: " + itemSheet.fullPath + '\\' + itemSheet.sheet);
                         }
                     }
+                    console.log("全部转换完成!");
                 },
                 _saveConfig() {
                     let data = {
