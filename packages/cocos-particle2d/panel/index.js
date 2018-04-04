ae5733f71b077515cdfb867dc7579129b9bf3b89
许彦峰 <774177933@qq.com>
2018-04-04 18:08:23 +0800

    no message

--- packages/cocos-particle2d/panel/index.js
+++ packages/cocos-particle2d/panel/index.js
@@ -7,7 +7,38 @@ Editor.Panel.extend({
     $: {},
 
     ready() {
+        new window.Vue({
+            el: this.shadowRoot,
+            created() {
 
+            },
+            init() {
+
+            },
+            data: {
+                test: ""
+            },
+            methods: {
+                onBtnClickTest() {
+                    console.log("1111");
+                    // let myScript = document.createElement("script");
+                    // myScript.type = "text/javascript";
+                    // myScript.appendChild(document.createTextNode("function functionOne(){alert(\"成功运行\"); }"));
+                    // document.body.appendChild(myScript);
+                    // functionOne();
+                },
+                onUpdateSaveName() {
+
+                },
+                onClickGen() {
+
+                },
+                onSelectImportPath() {
+
+                }
+
+            }
+        });
     },
 
     messages: {
